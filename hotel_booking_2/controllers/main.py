c2e1237b3b5691d4ede16a97d5cee24a1cb3791f
Henrik Norlin <henrik@appstogrow.co>
2023-05-22 22:06:35 +0200

    controller auth='user', hotel_booking.js with useService error (why?)

--- hotel_booking_2/controllers/main.py
+++ hotel_booking_2/controllers/main.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 
 class HotelBookingController(http.Controller):
-    @http.route(['/hotel_booking/ui'], type='http', auth='public')
+    @http.route(['/hotel_booking/ui'], type='http', auth='user')
     def hotel_booking_ui(self, hotel_folio_id, **k):
         context = {"hotel_folio_id": hotel_folio_id}
         return request.render('hotel_booking_2.hotel_booking_index', context)
--- hotel_booking_2/static/src/js/hotel_booking.js
+++ hotel_booking_2/static/src/js/hotel_booking.js
@@ -2,6 +2,7 @@
 
 import { Component } from "@odoo/owl";
 import { FloorScreen } from "@hotel_booking_2/js/FloorScreen/FloorScreen";
+import { useService } from "@web/core/utils/hooks";
 
 
 export class HotelBooking extends Component {
@@ -9,17 +10,37 @@ export class HotelBooking extends Component {
     static components = { FloorScreen };
 
     setup() {
+        //
+        // useService fails in hooks.js:192 const { services } = component.env; ENV IS MISSING, WHY???
+        //
+        // this.orm = useService("orm");
+        // this.rpc = useService("rpc");
         this.hotel_folio_id = odoo.hotel_folio_id;
-        this.floor = {
-            id: 1,
-            name: "Floor 1",
-            background_color: "orange",
-            pos_config_id: 1,
-            sequence: 1,
-            table_ids: [],
-        };
-        this.FloorScreen = this.floor;
-        this.isShown = true;
-        this.mobileSearchBarIsShown = false;
+        this.load_server_data();
+    }
+    async load_server_data(){
+        // const loadedData = await this.orm.call("hotel.folio", 'load_booking_data', [], {});
+        // await this._processData(loadedData);
+    }
+    async _processData(loadedData) {
+        this.floors = loadedData['hotel.floor'];
+        this.loadHotelFloor();
+    }
+    loadHotelFloor() {
+        // we do this in the front end due to the circular/recursive reference needed
+        // Ignore floorplan features if no floor specified.
+
+        // this.config.iface_floorplan = !!(this.floors && this.floors.length > 0);
+        // if (this.config.iface_floorplan) {
+            this.floors_by_id = {};
+            this.tables_by_id = {};
+            for (let floor of this.floors) {
+                this.floors_by_id[floor.id] = floor;
+                for (let table of floor.tables) {
+                    this.tables_by_id[table.id] = table;
+                    table.floor = floor;
+                }
+            }
+        // }
     }
 }
