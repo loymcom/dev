5abbc1512743982c7d17ea3ff58bc35127a51274
Henrik Norlin <henrik@appstogrow.co>
2023-05-26 11:23:46 +0200

    [ADD] dev

---
+++ dev/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Dev Menu",
+    "version": "1.0",
+    "summary": "Module Summary",
+    "description": "Module Description",
+    "category": "Administration",
+    "author": "Rick, Henrik",
+    "website": "Author Website",
+    "depends": [
+        "hotel",
+    ],
+    "data": [
+        "views/menus.xml",
+    ],
+    "license": "AGPL-3",
+}
