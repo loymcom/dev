3329cf7423693504eff804a8ce8cbd9a8db4a03e
Henrik Norlin <henrik@appstogrow.co>
2023-06-02 16:30:53 +0200

    product_variant_resource_booking: date filter

--- product_variant_resource_booking/models/product_product.py
+++ product_variant_resource_booking/models/product_product.py
@@ -1,6 +1,38 @@
+from datetime import datetime
+
 from odoo import api, fields, models
 
-class ResourceResource(models.Model):
-    _inherit = "resource.resource"
 
-    product_attribute_value_id = fields.Many2one("product.attribute.value", string="Product Attribute Value")
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    def _compute_dates(self):
+        date_start = None
+        date_end = None
+
+        context = self.env.context
+        date_format = self.env["res.lang"].search([("code", "=", context["lang"])]).date_format
+
+        domain = context.get("domain")
+        for d in domain:
+            if isinstance(d, list) or isinstance(d, tuple):
+                if isinstance(d[0], str) and d[0] == "date_start":
+                    date_start = d[2]
+                if isinstance(d[0], str) and d[0] == "date_end":
+                    date_end = d[2]
+        if date_start:
+            date_start = datetime.strptime(date_start, date_format)
+        if date_end:
+            date_end = datetime.strptime(date_end, date_format)
+
+        for record in self:
+            record.date_start = date_start
+            record.date_end = date_end
+
+    date_start = fields.Date(string="Start date", compute="_compute_dates")
+    date_end = fields.Date(string="End date", compute="_compute_dates")
+
+    @api.model
+    def web_search_read(self, domain=None, fields=None, offset=0, limit=None, order=None, count_limit=None):
+        self = self.with_context(domain=domain)
+        return super().web_search_read(domain, fields, offset, limit, order, count_limit)
