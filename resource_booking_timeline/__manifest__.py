7750d21087d4d77e1b09e24dd3f35bae41189d31
Henrik Norlin <henrik.norl@gmail.com>
2023-08-25 13:55:47 +0200

    Added resource_booking_timeline

---
+++ resource_booking_timeline/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Resource Booking Timeline",
+    "summary": "",
+    "author": "AppsToGROW, " "Odoo Community Association (OCA)",
+    "category": "Administration",
+    "data": [
+        "views/resource_booking_views.xml",
+    ],
+    "depends": [
+        "resource_booking",
+        "web_timeline",
+    ],
+    "license": "AGPL-3",
+    "maintainers": ["appstogrow"],
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/appstogrow/apps",
+}
