0b6352acf643d33a85befa9e38401616a560737d
Henrik Norlin <henrik.norl@gmail.com>
2023-08-25 14:16:36 +0200

    Added template for new modules

---
+++ template/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 AppsToGROW - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "TEMPLATE",
+    "summary": "",
+    "author": "AppsToGROW, Odoo Community Association (OCA)",
+    "category": "Uncategorized",
+    "data": [
+    ],
+    "depends": [
+        "",
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["appstogrow"],
+    "version": "14.0.1.0.0",
+    "website": "https://github.com/appstogrow/apps",
+}
