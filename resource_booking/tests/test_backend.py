Tecnativa/calendar

[FIX] resource_booking: correct assignment message
By using the upstream `mail.message_user_assigned` template, we were getting this exception:

```
odoo.addons.base.models.qweb.QWebException: 'resource.booking' object has no attribute 'user_id'
Traceback (most recent call last):
  File "/opt/odoo/custom/src/odoo/odoo/addons/base/models/qweb.py", line 348, in _compiled_fn
    return compiled(self, append, new, options, log)
  File "<template>", line 1, in template_2601_10519
AttributeError: 'resource.booking' object has no attribute 'user_id'
```

So I provide a specific template for this kind of subscription notification, which doesn't use that `user_id` field. Besides, it explains better why you're getting it.

@Tecnativa TT32190

resource_booking/tests/test_backend.py
@@ -6,6 +6,7 @@ from odoo.exceptions import ValidationError
 from odoo import fields
 from datetime import datetime
 from .common import create_test_data
+from unittest.mock import patch
 
 _2dt = fields.Datetime.to_datetime
 
@@ -184,7 +185,7 @@ class BackendCase(SavepointCase):
         self.assertTrue(booking.combination_id)
 
     def test_state(self):
-        # I create a pending booking
+        # I create a pending bookingpatch
         booking = self.env["resource.booking"].create(
             {"type_id": self.rbt.id, "partner_id": self.partner.id}
         )
@@ -384,17 +385,19 @@ class BackendCase(SavepointCase):
         rb_user = new_test_user(
             self.env, login="rbu", groups="base.group_user,resource_booking.group_user"
         )
-        rb = (
-            self.env["resource.booking"]
-            .sudo(rb_user)
-            .create(
-                {
-                    "partner_id": self.partner.id,
-                    "type_id": self.rbt.id,
-                    "combination_id": self.rbcs[0].id,
-                }
+        # Enable auto-subscription messaging
+        with patch.object(self.env.registry, "ready", True):
+            rb = (
+                self.env["resource.booking"]
+                .sudo(rb_user)
+                .create(
+                    {
+                        "partner_id": self.partner.id,
+                        "type_id": self.rbt.id,
+                        "combination_id": self.rbcs[0].id,
+                    }
+                )
             )
-        )
         # Creator and resource must already be following
         self.assertEqual(
             rb.message_partner_ids, rb_user.partner_id | self.users[0].partner_id

loymcom/dev

resource_booking/tests/test_backend.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from datetime import date, datetime
+from unittest.mock import patch
 
 from freezegun import freeze_time
 from pytz import utc
@@ -594,19 +595,21 @@ class BackendCase(SavepointCase):
         rb_user = new_test_user(
             self.env, login="rbu", groups="base.group_user,resource_booking.group_user"
         )
-        rb = (
-            self.env["resource.booking"]
-            .with_user(rb_user)
-            .create(
-                {
-                    "partner_id": self.partner.id,
-                    "type_id": self.rbt.id,
-                    "combination_auto_assign": False,
-                    "combination_id": self.rbcs[0].id,
-                    "user_id": self.users[1].id,
-                }
+        # Enable auto-subscription messaging
+        with patch.object(self.env.registry, "ready", True):
+            rb = (
+                self.env["resource.booking"]
+                .with_user(rb_user)
+                .create(
+                    {
+                        "partner_id": self.partner.id,
+                        "type_id": self.rbt.id,
+                        "combination_auto_assign": False,
+                        "combination_id": self.rbcs[0].id,
+                        "user_id": self.users[1].id,
+                    }
+                )
             )
-        )
         # Organizer, combination and creator must already be following
         self.assertEqual(
             rb.message_partner_ids, rb_user.partner_id | self.users[:2].partner_id