43cfa5345b4451e258c636ebc52f3af8e52ccf6b
Henrik Norlin <henrik.norl@gmail.com>
2023-08-30 23:25:04 +0200

    sale_resource_booking_product_variant: DONE

--- sale_resource_booking_product_variant/__manifest__.py
+++ sale_resource_booking_product_variant/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2023 AppsToGROW - Henrik Norlin
+# Copyright 2023 Ows - Henrik Norlin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Sell Product Variants with Resources",
     "summary": "",
-    "author": "AppsToGROW, Odoo Community Association (OCA)",
-    "category": "Uncategorized",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "Appointments",
     "data": [
         "views/product_attribute_views.xml",
         "views/product_product_views.xml",
@@ -18,9 +18,9 @@
     ],
     "development_status": "Alpha",
     "license": "AGPL-3",
-    "maintainers": ["appstogrow"],
+    "maintainers": ["ows-cloud"],
     "post_init_hook": "post_init_hook",
     "uninstall_hook": "uninstall_hook",
     "version": "16.0.1.0.0",
-    "website": "https://github.com/appstogrow/apps",
+    "website": "https://github.com/OCA/sale-workflow",
 }
