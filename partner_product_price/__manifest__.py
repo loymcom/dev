35cce33b9eb96c2da36ff7184ac2cd942a34d140
Henrik Norlin <henrik.norl@gmail.com>
2023-08-30 21:30:19 +0200

    partner_product_price: DONE

--- partner_product_price/__manifest__.py
+++ partner_product_price/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2023 AppsToGROW - Henrik Norlin
+# Copyright 2023 Ows - Henrik Norlin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Contact -> Products with the contact's prices",
     "summary": "",
-    "author": "AppsToGROW, Odoo Community Association (OCA)",
-    "category": "Uncategorized",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "Product",
     "data": [
         "data/ir_actions_server.xml",
         "views/product_product_views.xml",
@@ -15,7 +15,7 @@
     ],
     "development_status": "Alpha",
     "license": "AGPL-3",
-    "maintainers": ["appstogrow"],
+    "maintainers": ["ows-cloud"],
     "version": "16.0.1.0.0",
-    "website": "https://github.com/appstogrow/apps",
+    "website": "https://github.com/oca/product-attribute",
 }
