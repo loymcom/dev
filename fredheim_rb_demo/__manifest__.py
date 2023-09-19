53724d9b04ad874bf7358d19624ab2ee07946f87
Henrik Norlin <henrik.norl@gmail.com>
2023-09-19 14:18:53 +0200

    [ADD] fredheim_rb_demo

---
+++ fredheim_rb_demo/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Ows - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fredheim Resource Booking Demo",
+    "summary": "",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "Uncategorized",
+    "demo": [
+        "demo/demo.xml",
+    ],
+    "depends": [
+        "sale_resource_booking_product_variant",
+        # "sale_resource_booking_timeline",
+        # "website_sale_resource_booking",
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["ows-cloud"],
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/",
+}
