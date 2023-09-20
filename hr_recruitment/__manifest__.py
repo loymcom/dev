cc962759abeee92febb06347a8857614cdb245be
Henrik Norlin <henrik.norl@gmail.com>
2023-09-20 08:34:17 +0200

    [DUMMY] hr_recruitment

---
+++ hr_recruitment/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Ows - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "DUMMY hr_recruitment",
+    "summary": "",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "Uncategorized",
+    "data": [
+        "views/my_model_views.xml",
+    ],
+    "depends": [
+        "base",
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["ows-cloud"],
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/",
+}
---
+++ hr_recruitment/models/my_model.py
@@ -0,0 +1,4 @@
+from odoo import api, fields, models
+
+class MyModel(models.Model):
+    _inherit = "my.model"
