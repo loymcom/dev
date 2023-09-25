c864a130a7e51a053a988153cafce505c96164c2
Henrik Norlin <henrik.norl@gmail.com>
2023-09-25 12:18:17 +0200

    [FIX] demo & _get_available_slots()

--- sale_resource_booking_period/models/resource_booking.py
+++ sale_resource_booking_period/models/resource_booking.py
@@ -1,4 +1,5 @@
 import pytz
+from collections import defaultdict
 from datetime import timedelta
 
 from odoo import api, fields, models
@@ -17,8 +18,11 @@ class ResourceBooking(models.Model):
         result = super()._get_available_slots(start_dt, end_dt)
         # If the product has booking periods,
         # then restrict to these periods.
+        # dt = datetime
+        # dts = list of datetimes
         booking_periods = self.product_id.booking_period_ids
         if booking_periods:
+            new_result = defaultdict(list)
             for result_date, result_dts in result.items():
                 new_dts = []
                 for result_dt in result_dts:
@@ -26,6 +30,6 @@ class ResourceBooking(models.Model):
                     stop = start + timedelta(hours=self.duration)
                     for period in booking_periods:
                         if period.start <= start < period.stop and period.start < stop <= period.stop:
-                            new_dts.append(result_dt)
-                result[result_date] = new_dts
+                            new_result[result_date].append(result_dt)
+            result = new_result
         return result
