1bf39336e1b2cd7ebfcb19569f36b469191206f5
Henrik Norlin <henrik.norl@gmail.com>
2023-10-15 23:31:25 +0200

    [FIX] app_creator: test.csv -> new app that works well

--- app_creator/app_creator2.py
+++ app_creator/app_creator2.py
@@ -2,11 +2,6 @@ import csv
 import os
 from pprint import pprint
 
-# from app_templates import t_manifest, t_manifest_data
-# from app_templates import t_access_header, t_access_line, t_init, t_model
-# from app_templates import t_model_field, t_model_field_attr, t_model_field_attr_quote
-# from app_templates import t_xml, t_view, t_sheet, t_group, t_field, t_action
-# from app_templates import t_menu_main, t_menu_item
 import app_templates as t
 
 
@@ -14,111 +9,73 @@ csv_file = "test.csv"
 csv_delimiter = ";"
 app_parent_path = ".."
 
+# We say "app" instead of "module".
+
+# model_dot = "model.name"
+# model_underscore = "model_name"
+
 
 def create_app_main():
-    module, menus, models, actions, accesses, fields = csv2lists(csv_file)
-    lists2app(module, menus, models, actions, accesses, fields)
+    app, models, fields = csv2lists(csv_file)
+    lists2app(app, models, fields)
 
 
 def csv2lists(csv_file):
-    module = {}
-    menus = []
+    app = {}
     models = []
-    actions = []
-    accesses = []
     fields = []
-    model_tech = ""
+    model_dot = ""
 
     with open(csv_file) as f:
         reader = csv.DictReader(f, delimiter=csv_delimiter, quotechar='"')
         lines = [d for d in reader]
         for counter, line in enumerate(lines):
-            line = line_desc2tech(line)
+            line = line_add_defaults(line)
 
-            if line.get("module.tech"):
-                module = {
-                    "name": line["module.tech"],
-                    "shortdesc": line["module.desc"]
-                }
-                # menus.append(
-                #     {
-                #         "name": line["module.desc"],
-                #         "parent_id": line.get("module.parent_menu_extid"),
-                #         "extid": line["module.desc"] + "_action",
-                #     }
-                # )
+            if line.get("app.name"):
+                app = line_filter(line, "app.", [])
 
-            if line.get("model.tech"):
-                model_tech = line["model.tech"]
+            if line.get("model.name"):
+                model_dot = line["model.dot"]
+                line["model.sequence"] = counter
                 models.append(
-                    {
-                        "model": line["model.tech"],
-                        "name": line["model.desc"],
-                        "context": {
-                            "sequence": counter,
-                            "group_extid": (
-                                line.get("model.group_extid") or "base.group_user"
-                            ),
-                        },
-                    }
+                    line_filter(line, "model.", [])
                 )
-                # menus.append(
-                #     {
-                #         "name": line["model.desc"],
-                #         "action": line["model.tech"],
-                #         "sequence": counter,
-                #         "parent_id": "{}.{}"
-                #     }
-                # )
-                # actions.append(
-                #     {
-                #         "name": line["model.desc"],
-                #         "res_model": line["model.tech"],
-                #         # "view_mode": line.get("model.views") or "tree,form"
-                #     }
-                # )
-                # accesses.append(
-                #     {
-                #         "name": ["model.tech"],
-                #         "group_id": line.get("model.group_extid") or "base.group_user",
-                #     }
-                # )
 
+            line["field.sequence"] = counter
+            line["model.dot"] = model_dot
             fields.append(
-                {
-                    "name": line["field.tech"],
-                    "field_description": line["field.desc"],
-                    "ttype": line["field.type"],
-                    "relation": line.get("field.relation"),
-                    "groups": line.get("field.group_extid"),
-                    "model": model_tech,
-                    "context": {
-                        "sequence": counter,
-                        # "field_group": line.get("field_group"),
-                    },
-                }
+                line_filter(line, "field.", ["model.dot"])
             )
-    return module, menus, models, actions, accesses, fields
+    return app, models, fields
 
 
-def line_desc2tech(line):
-    desc2tech = [
-        ("module.desc", "module.tech", "_"),
-        ("model.desc", "model.tech", "."),
-        # ("model.desc", "model.tech", "_"),
-        ("field.desc", "field.tech", "_"),
-     ]
-    for desc, tech, delimiter in desc2tech:
-        if line.get(desc) and not line.get(tech):
-            line[tech] = line[desc].replace(" ", delimiter).lower()
+def line_add_defaults(line):
+    if_first_not_second = [
+        ("app.name", "app.underscore", " ", "_"),
+        ("model.name", "model.dot", " ", "."),
+        ("model.name", "model.underscore", " ", "_"),
+        ("field.name", "field.underscore", " ", "_"),
+        ("model.name", "model.group_extid", line["model.name"], "base.group_user"),
+    ]
+    for first, second, replace, with_value in if_first_not_second:
+        if line.get(first) and not line.get(second):
+            line[second] = line[first].replace(replace, with_value).lower()
     return line
 
 
-def lists2app(module, menus, models, actions, accesses, fields):
+def line_filter(line, prefix, keys):
+    result = {}
+    for key, value in line.items():
+        if key.startswith(prefix) or key in keys:
+            result[key] = value
+    return result
+
+
+def lists2app(app, models, fields):
 
     # DIRECTORIES
-
-    app_path = "{}/{}".format(app_parent_path, module["name"])
+    app_path = "{}/{}".format(app_parent_path, app["app.underscore"])
     os.makedirs(app_path, exist_ok=True)
     os.makedirs(app_path + "/models", exist_ok=True)
     os.makedirs(app_path + "/security", exist_ok=True)
@@ -128,11 +85,11 @@ def lists2app(module, menus, models, actions, accesses, fields):
 
     data = [
         t.manifest_data.format(
-            model=model["model"].replace(".", "_")
+            _model_=model["model.underscore"]
         ) for model in models
     ]
-    manifest  = t.manifest.format(
-        shortdesc=module["shortdesc"],
+    manifest = t.manifest.format(
+        app_name=app["app.name"],
         data="".join(data),
     )
     with open(app_path + "/__manifest__.py", "w") as f:
@@ -141,25 +98,21 @@ def lists2app(module, menus, models, actions, accesses, fields):
     # INIT
 
     with open(app_path + "/__init__.py", "w") as f:
-        f.write(t.init.format(model="model"))
+        f.write(t.init.format(_model_="models"))
 
     with open(app_path + "/models/__init__.py", "w") as f:
         for model in models:
-            f.write(t.init.format(model=model["model"]))
+            f.write(t.init.format(_model_=model["model.underscore"]))
 
     # MODELS
 
     for model in models:
-        classname = model["model"].title().replace(".", "")
-        modelname = model["model"]
-        fields_py = get_fields_py(model, fields)
-
-        path = "{}/models/{}.py".format(app_path, model["model"].replace(".", "_"))
+        path = app_path + "/models/" + model["model.underscore"] + ".py"
         with open(path, "w") as f:
             f.write(t.model.format(
-                classname=classname,
-                modelname=modelname,
-                fields_py=fields_py,
+                Model=model["model.dot"].title().replace(".", ""),
+                model=model["model.dot"],
+                fields_py=get_fields_py(model, fields),
             ))
 
     # ACCESS
@@ -167,104 +120,92 @@ def lists2app(module, menus, models, actions, accesses, fields):
     with open(app_path + "/security/ir.model.access.csv", "w") as f:
         f.write(t.access_header)
         for model in models:
-            model_name = model["model"]
-            _model_name_ = model_name.replace(".", "_")
-            group_extid = model["context"]["group_extid"]
             f.write(t.access_line.format(
-                model=model,
-                _model_=_model_name_,
-                group_extid=group_extid,
+                model=model["model.dot"],
+                _model_=model["model.underscore"],
+                group_extid=model["model.group_extid"],
             ))
-        # for access in accesses:
-        #     model = access["name"]
-        #     _model_ = model.replace(".", "_")
-        #     group_extid = access["group_id"]
-        #     f.write(t.access_line.format(
-        #         model=model,
-        #         _model_=_model_,
-        #         group_extid=group_extid,
-        #     ))
 
     # XML
 
     for model in models:
         model_views_xml = get_model_views_xml(model, fields)
-        _model_ = model["model"].replace(".", "_")
-        with open(app_path + "/views/" + _model_ + "_views.xml", "w") as f:
+        path = app_path + "/views/" + model["model.underscore"] + "_views.xml"
+        with open(path, "w") as f:
             f.write(model_views_xml)
 
-    menus_xml = get_menus_xml(module, models)
+    menus_xml = get_menus_xml(app, models)
     with open(app_path + "/views/menus.xml", "w") as f:
         f.write(menus_xml)
 
 
 def get_fields_py(model, fields):
     fields_py = []
-    for counter, field in enumerate(fields):
-        if field["model"] == model["model"]:
-
-            # if counter == 0 or field["context"].get("field_group"):
-            #     fields_py.append("\n")
+    for field in fields:
+        if field["model.dot"] == model["model.dot"]:
 
             def do_attr(attr, key, quote=False):
-                # attr = python method attribute
+                # attr = python field attribute
                 # key = csv column dict key
                 # quote = boolean, should the value be inside quotes ("value")?
-                if key in field:
+                if key in field and field.get(key):
                     line = t.model_field_attr_quote if quote else t.model_field_attr
                     attrs_py.append(line.format(
                         attr=attr, value=field[key])
                     )
             attrs_py = []
-            do_attr("string", "field_description")
-            do_attr("comodel_name", "relation")
-            do_attr("groups", "groups")
+            do_attr("string", "field.name", quote=True)
+            do_attr("comodel_name", "field.relation", quote=True)
+            do_attr("groups", "field.groups", quote=True)
 
             fields_py.append(t.model_field.format(
-                field=field["name"],
-                ttype=field["ttype"].capitalize(),
-                attrs=attrs_py,
+                field=field["field.underscore"],
+                ttype=field["field.type"].capitalize(),
+                attrs="".join(attrs_py),
             ))
     return "".join(fields_py)
 
 
 def get_model_views_xml(model, fields):
-    model_name = model["model"]
-    _model_name_ = model_name.replace(".", "_")
+    model_name = model["model.name"]
+    model_dot = model["model.dot"]
+    model_underscore = model["model.underscore"]
     form_fields = []
     tree_fields = []
     search_fields = []
     show = 'optional="show" '
 
     for field in fields:
-        if field["model"] == model["model"]:
-            form_fields.append(t.field.format(field=field["name"], extra=""))
-            tree_fields.append(t.field.format(field=field["name"], extra=show))
-            search_fields.append(t.field.format(field=field["name"], extra=""))
+        if field["model.dot"] == model_dot:
+            fld_underscore = "field.underscore"
+            form_fields.append(t.field.format(field=field[fld_underscore], extra=""))
+            tree_fields.append(t.field.format(field=field[fld_underscore], extra=show))
+            search_fields.append(t.field.format(field=field[fld_underscore], extra=""))
 
     form_group = t.group.format(content="".join(form_fields))
     form_sheet = t.sheet.format(content=form_group)
     form_view = t.view.format(
-        model=model_name,
-        _model_=_model_name_,
+        model=model_dot,
+        _model_=model_underscore,
         view="form",
         content=form_sheet,
     )
     tree_view = t.view.format(
-        model=model_name,
-        _model_=_model_name_,
+        model=model_dot,
+        _model_=model_underscore,
         view="tree",
         content="".join(tree_fields),
     )
     search_view = t.view.format(
-        model=model_name,
-        _model_=_model_name_,
+        model=model_dot,
+        _model_=model_underscore,
         view="search",
         content="".join(search_fields),
     )
     action = t.action.format(
-        model=model_name,
-        _model_=_model_name_,
+        model_name=model_name,
+        model=model_dot,
+        _model_=model_underscore,
     )
     xml = t.xml.format(
         content=form_view + tree_view + search_view + action
@@ -272,26 +213,24 @@ def get_model_views_xml(model, fields):
     return xml
 
 
-def get_menus_xml(module, models):
+def get_menus_xml(app, models):
     menus_xml = []
-    module_desc = module["shortdesc"]
-    _module_ = module["name"]
-    parent_menu_extid = module.get("parent_id")
+
     menus_xml.append(t.menu_main.format(
-        module_desc=module_desc,
-        _module_=_module_,
-        parent_menu_extid=parent_menu_extid,
+        app_name=app["app.name"],
+        app=app["app.underscore"],
+        parent_menu_extid=app.get("parent_menu_extid", ""),
     ))
 
     for model in models:
         menus_xml.append(t.menu_item.format(
-            model_desc=model["name"],
-            _model_=model["model"].replace(".", "_"),
-            _module_=_module_,
-            sequence=model["context"]["sequence"],
+            app=app["app.underscore"],
+            _model_=model["model.underscore"],
+            model_name=model["model.name"],
+            sequence=model["model.sequence"],
         ))
 
-    return "".join(menus_xml)
+    return t.xml.format(content="".join(menus_xml))
 
 
 create_app_main()
--- app_creator/app_templates.py
+++ app_creator/app_templates.py
@@ -1,8 +1,9 @@
 manifest = """
 {{
-    "name": "{shortdesc}",
+    "name": "{app_name}",
     "data": [{data}
         "views/menus.xml",
+        "security/ir.model.access.csv",
     ],
     "depends": [
         "base",
@@ -11,17 +12,16 @@ manifest = """
 }}
 """
 
-manifest_data = """\n        "views/{model}_views.xml","""
+manifest_data = """\n        "views/{_model_}_views.xml","""
 
 # MODEL
 
-init = "from . import {model}\n"
+init = "from . import {_model_}\n"
 
 model = """from odoo import api, fields, models
 
-class {classname}(models.Model):
-    _name = "{modelname}"
-
+class {Model}(models.Model):
+    _name = "{model}"
 {fields_py}
 """
 
@@ -32,65 +32,56 @@ model_field_attr_quote = """\n        {attr}="{value}","""
 
 # ACCESS
 
-access_header = "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink,perm_export"
-access_line = "access_{_model_},{model},model_{_model_},{group_extid},1,1,1,1,1"
+access_header = "id,name,model_id:id,group_id:id,perm_read,perm_write,perm_create,perm_unlink\n"
+access_line = "access_{_model_},{model},model_{_model_},{group_extid},1,1,1,1\n"
 
 # XML
 
-xml = """
-<?xml version="1.0" encoding="utf-8" ?>
+xml = """<?xml version="1.0" encoding="utf-8" ?>
 <odoo>{content}
 </odoo>
 """
 
 view = """
-    <record if="{_model_}_view_{view}" model="ir.ui.view">
+    <record id="{_model_}_view_{view}" model="ir.ui.view">
         <field name="name">{model}.view.form</field>
         <field name="model">{model}</field>
         <field name="arch" type="xml">
             <{view}>{content}
             </{view}>
         </field>
-    </record>
-"""
+    </record>"""
 
 sheet = """
                 <sheet>{content}
-                </sheet>
-"""
+                </sheet>"""
 
 group = """
                     <group>{content}
-                    </group>
-"""
+                    </group>"""
 
 field = """
-                        <field name="{field}" {extra}/>
-"""
+                        <field name="{field}" {extra}/>"""
 
 action = """
     <record id="{_model_}_action" model="ir.actions.act_window">
-        <field name="name">{model} action</field>
+        <field name="name">{model_name}</field>
         <field name="res_model">{model}</field>
         <field name="view_mode">tree,form</field>
-    </record>
-"""
+    </record>"""
 
 menu_main = """
     <menuitem
-        id="{_module_}_main_menu"
+        id="{app}_main_menu"
         parent="{parent_menu_extid}"
-        string="{module_desc}"
-    />
-"""
+        name="{app_name}"
+    />"""
 
 menu_item = """
     <menuitem
         id="{_model_}_menu"
         action="{_model_}_action"
-        parent="{_module_}_main_menu"
-        string="{model_desc}"
+        parent="{app}_main_menu"
+        name="{model_name}"
         sequence="{sequence}"
-    />
-"""
-
+    />"""
