786252bfb48694c9b8da08537885f8aa5d010c9b
Henrik Norlin <henrik@loym.com>
2024-06-18 09:41:34 +0200

    team_m csv to contact record

--- team_m/models/team_m.py
+++ team_m/models/team_m.py
@@ -1,3 +1,6 @@
+import csv
+import io
+
 from odoo import api, fields, models
 from odoo.exceptions import UserError
 
@@ -12,4 +15,31 @@ class TeamM(models.Model):
     state = fields.Selection([("new", "New"), ("done", "Done")], default="new")
 
     def action_team_m(self):
-        raise UserError("Test Error")
+        csv_file_like = io.StringIO(self.csv.strip())
+        csv_reader = csv.DictReader(csv_file_like)
+
+        Partner = self.env["res.partner"]
+        contact = None
+        for values in csv_reader:
+            contact_name = values["firstname"] + " " + values["lastname"]
+            contact = Partner.search([("name", "=", contact_name)])
+            # .write(values)
+            # .unlink()
+            if not contact:
+                contact = Partner.create(values)
+
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "res.partner",
+            "views": [[False, "form"]],
+            "res_id": contact[0].id,
+            "target": "new",
+        }
+
+        # Iterate over the rows in the DictReader
+        mylist = []
+        for row in csv_reader:
+            # Each row is a dictionary
+            mylist.append(row)
+        raise UserError(str(mylist))
+
